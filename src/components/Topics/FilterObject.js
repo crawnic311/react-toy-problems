3cb5886b584e3219cee4d2d1b9542de600f06f90
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2021-11-17 22:21:19 -0700

    FilterObject Component built

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,9 +1,57 @@
 export default class FilterObject extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            unfilteredArray: [
+                {
+                 name: "Cloud",
+                 game: "FF7",
+                 age: 24,
+                },
+                {
+                 name: "Tifa",
+                 game: "FF7",
+                 age: 22,
+                },
+                {
+                 name: "Wakka",
+                 game: "FF10",
+                 age: 32
+                }
+            ],
+            
+            userInput = "",
+            filteredArray = []
+        }
+    }
+
+    handleChange(e) {
+        this.setState({userInput: e})
+    }
+
+    selectCharacter(prop) {
+        let characters = this.state.unfilteredArray
+        let filteredArray = []
+
+        for(let i = 0; i < unfilteredArray.length; i++) {
+            if(employees[i].hasOwnProperty(prop)) {
+                filteredArray.push(unfilteredArray[i])
+            }
+        }
+        
+        this.setState({filteredArray: filteredArray})
+    }
+
     render() {
         return (
-            <p>
-                "FilterObject"
-            </p>
+            <div className="puzzleBox filterObjectPB">
+                <h4>"Filter Object</h4>
+                <span className="puzzleText">Characters: {JSON.stringify(this.state.unfilteredArray)}</span>
+                <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value)}></input>
+                <button className="confirmationButton" onClick={ () => selectCharacter(this.state.userInput)}></button>
+                <span className="resultsBox filterObjectRB">Selected Characters: {JSON.stringify(this.state.filteredArray)}</span>
+            </div>
         )
     }
 }
