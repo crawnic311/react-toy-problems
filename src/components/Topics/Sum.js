4c8cbd8af2e773b105392686a3ea07875c607394
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2021-11-17 22:58:00 -0700

    Finished

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,9 +1,36 @@
 export default class Sum extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            number1: 0,
+            number2: 0,
+            sum: null
+        }
+    }
+
+    changeHandler1(e) {
+        this.setState({number1: parseInt(e, 10)})
+    }
+    
+    changeHandler2(e) {
+        this.setState({number2: parseInt(e, 10)})
+    }
+
+    sumNums(num1, num2) {
+        this.setState({sum: num1 + num2})
+    }
+
     render() {
         return (
-            <p>
-                "Sum"
-            </p>
+            <div className="puzzleBox sumPB">
+                <h4>Sum</h4>
+                <input className="inputLine" onChange={ (e) => changeHandler1(e.target.value)}></input>
+                <input className="inputLine" onChange={ (e) => this.changeHandler2(e.target.value)}></input>
+                <button className="confirmationButton" onClick={ () => sumNums(this.state.number1, this.state.number2)}></button>
+                <span className="resultsBox">Sum: {JSON.stringify(this.state.sum)}</span>
+
+            </div>
         )
     }
 }
